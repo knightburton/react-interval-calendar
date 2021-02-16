f7988b1dd4f0853990485edec3633a45184dd425
Imre Kiss <jakab.asd@gmail.com>
2021-02-16 23:25:53 +0100

    Calculate and add the number of weeks to the context form the main component

--- src/components/IntervalCalendar/index.jsx
+++ src/components/IntervalCalendar/index.jsx
@@ -4,32 +4,62 @@ import PropTypes from 'prop-types';
 import IntervalCalendarHeader from '../IntervalCalendarHeader';
 
 import Context from '../../context';
+import {
+  getMonthStart,
+  getMonthEnd,
+  getWeekStart,
+  getWeekEnd,
+  getDifferenceInCalendarWeeks,
+} from '../../utils/date';
 import { WEEKDAY_KEYS } from '../../constants';
 
-const IntervalCalendar = ({ showHeader, showWeekdays, weekStartsOn }) => {
+const IntervalCalendar = ({
+  start,
+  end,
+  showHeader,
+  showWeekdays,
+  weekStartsOn,
+}) => {
   // use memo hooks
+  const [, , numberOfWeeks] = useMemo(() => {
+    if (start && end && weekStartsOn) {
+      const monthStart = getMonthStart(start);
+      const monthEnd = getMonthEnd(end);
+      const alfa = getWeekStart(monthStart, weekStartsOn);
+      const omega = getWeekEnd(monthEnd, weekStartsOn);
+      const weeks = getDifferenceInCalendarWeeks(omega, alfa, weekStartsOn);
+
+      return [alfa, omega, weeks];
+    }
+    return [null, null, null];
+  }, [start, end, weekStartsOn]);
+
   const contextValue = useMemo(() => ({
+    numberOfWeeks,
     showWeekdays,
     weekStartsOn,
-  }), [showWeekdays, weekStartsOn]);
+  }), [numberOfWeeks, showWeekdays, weekStartsOn]);
 
   return (
     <Context.Provider value={contextValue}>
       <div>
         {showHeader && <IntervalCalendarHeader />}
-        <p>Calendar</p>
       </div>
     </Context.Provider>
   );
 };
 
 IntervalCalendar.propTypes = {
+  start: PropTypes.instanceOf(Date),
+  end: PropTypes.instanceOf(Date),
   showHeader: PropTypes.bool,
   showWeekdays: PropTypes.bool,
   weekStartsOn: PropTypes.oneOf(WEEKDAY_KEYS),
 };
 
 IntervalCalendar.defaultProps = {
+  start: null,
+  end: null,
   showHeader: true,
   showWeekdays: true,
   weekStartsOn: 0,
--- src/context/index.js
+++ src/context/index.js
@@ -1,6 +1,7 @@
 import { createContext } from 'react';
 
 const Context = createContext({
+  numberOfWeeks: 0,
   showWeekdays: true,
   weekStartsOn: 0,
 });
