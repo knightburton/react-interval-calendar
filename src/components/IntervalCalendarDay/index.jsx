e02314766b52ad62543f4551c67843c021e491ba
Imre Kiss <jakab.asd@gmail.com>
2021-02-17 21:53:23 +0100

    Add the base of the calendar day component

---
+++ src/components/IntervalCalendarDay/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './styles.less';
+
+const IntervalCalendarDay = ({ day }) => {
+  // prop destruction
+  const { display } = day;
+
+  return (
+    <li className={styles.day}>
+      {display}
+    </li>
+  );
+};
+
+IntervalCalendarDay.propTypes = {
+  day: PropTypes.shape({
+    key: PropTypes.string,
+    date: PropTypes.instanceOf(Date),
+    display: PropTypes.number,
+  }),
+};
+
+IntervalCalendarDay.defaultProps = {
+  day: {},
+};
+
+export default IntervalCalendarDay;
