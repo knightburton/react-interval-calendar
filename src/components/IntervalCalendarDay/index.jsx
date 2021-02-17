3b73e9fdf88ad68c4c8118d08944cea3ff61ca2a
Imre Kiss <jakab.asd@gmail.com>
2021-02-17 23:51:49 +0100

    Color the day cell backgrounds based on the date month parity

--- src/components/IntervalCalendarDay/index.jsx
+++ src/components/IntervalCalendarDay/index.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
+
+import classnames from '../../utils/classnames';
 
 import styles from './styles.less';
 
 const IntervalCalendarDay = ({ day }) => {
   // prop destruction
-  const { display } = day;
+  const { display, monthEven } = day;
+
+  // useMemo hooks
+  const className = useMemo(
+    () => classnames(
+      styles.day,
+      {
+        [styles.day__month__even]: monthEven,
+      },
+    ),
+    [monthEven],
+  );
 
   return (
-    <li className={styles.day}>
+    <li className={className}>
       {display}
     </li>
   );
@@ -19,6 +32,7 @@ IntervalCalendarDay.propTypes = {
     key: PropTypes.string,
     date: PropTypes.instanceOf(Date),
     display: PropTypes.string,
+    monthEven: PropTypes.bool,
   }),
 };
 
--- src/components/IntervalCalendarWeek/index.jsx
+++ src/components/IntervalCalendarWeek/index.jsx
@@ -7,6 +7,7 @@ import Context from '../../context';
 import {
   addDays,
   addWeeks,
+  getMonthEven,
   formatDate,
 } from '../../utils/date';
 import styles from './styles.less';
@@ -24,6 +25,7 @@ const IntervalCalendarWeek = ({ numberOfWeek }) => {
         key: `${numberOfWeek}-${day}`,
         date,
         display: formatDate(date),
+        monthEven: getMonthEven(date),
       };
     }),
     [numberOfWeek, startDate],
