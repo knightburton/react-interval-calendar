883b4ba6cdfc1c1beef53aa64eb4e49e221c905f
Imre Kiss <jakab.asd@gmail.com>
2021-02-22 22:06:50 +0100

    Update example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -8,6 +8,13 @@ const App = () => (
       weekStartsOn={1}
       start={new Date(2021, 1, 1)}
       end={new Date(2021, 6, 31)}
+      highlighted={[
+        { key: 'holiday', start: new Date(2021, 1, 16), end: new Date(2021, 1, 20) },
+        { key: 'cleaning', start: new Date(2021, 2, 24), end: new Date(2021, 3, 9), color: '#00ff00' },
+      ]}
+      height={700}
+      showBorder
+      showBorderRadius
       fadeWeekends
     />
   </div>
