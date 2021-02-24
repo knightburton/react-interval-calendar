07c65ad6458f69bce7b5a60fd9b123e1eb3ade22
Imre Kiss <jakab.asd@gmail.com>
2021-02-25 00:02:09 +0100

    Improve the color utils and fix issues around rgba replaces

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -1,7 +1,16 @@
-const HEX_COLOR_REGEX = /^(#|0x)([\da-f]{2})([\da-f]{2})([\da-f]{2})$/;
+const HEX_REGEX = /^[\da-fA-F]*$/;
+const HEX_COLOR_REGEX = /^(#|0x)([\da-fA-F]{2})([\da-fA-F]{2})([\da-fA-F]{2})$/;
 const RGBA_COLOR_REGEX = /^rgb(a)?\s*?\(\s*?(000|0?\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\s*?,\s*?(000|0?\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\s*?,\s*?(000|0?\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\s*?(,\s*?(0|0\.\d*|1|1.0*)\s*?)?\)$/;
 const RGB_OPENING_REGEX = /^rgb/;
 const RGB_CLOSING_REGEX = /\)$/;
+
+/**
+ * Converts the hexadecimal string to number.
+ *
+ * @param hex Hexadecimal value to convert.
+ * @param defaultValue Return value in case of invalid hex.
+ */
+export const convertHexToInteger = (hex?: string | null, defaultValue = 0): number => (hex && HEX_REGEX.test(hex) ? parseInt(hex, 16) : defaultValue);
 
 /**
  * Converts the given hex, rgb color to rgba format
@@ -17,28 +26,21 @@ export const convertColorToRgba = (color: string, alpha = 0.2): string => {
   if (!rgba && !hex) throw new Error(`${color} is not in Hexadecimal, RGB or RGBA Color format.`);
   if (alpha < 0 || alpha > 1) throw new Error(`${alpha} is not from 0 to 1 range for rgba.`);
 
-  // The given color is in rgb or rgba format.
-  if (rgba) {
-    // The given color is in full rgba format.
-    // There is no need to touch the color.
-    if (rgba[1] && rgba[6]) return color;
-    // The given color is almost full rgba format only alpha missing.
-    // Append the alpha argument to it.
-    if (rgba[1] && !rgba[6]) return color.replace(RGB_CLOSING_REGEX, `, ${alpha}`);
-    // The given color is is rgb format.
-    // Append the definition and the alpha value to it.
-    if (!rgba[1] && !rgba[6]) return color.replace(RGB_OPENING_REGEX, 'rgba').replace(RGB_CLOSING_REGEX, `, ${alpha}`);
-  }
+  // The given color is in full rgba format.
+  // There is no need to touch the color.
+  if (rgba && rgba[1] && rgba[6]) return color;
+  // The given color is almost full rgba format only alpha missing.
+  // Append the alpha argument to it.
+  if (rgba && rgba[1] && !rgba[6]) return color.replace(RGB_CLOSING_REGEX, `, ${alpha})`);
+  // The given color is is rgb format.
+  // Append the definition and the alpha value to it.
+  if (rgba && !rgba[1] && !rgba[6]) return color.replace(RGB_OPENING_REGEX, 'rgba').replace(RGB_CLOSING_REGEX, `, ${alpha})`);
 
-  // The given color is in proper hex format.
-  if (hex && hex[2] && hex[3] && hex[4]) {
-    const red = parseInt(hex[2], 16) || 0;
-    const green = parseInt(hex[3], 16) || 0;
-    const blue = parseInt(hex[4], 16) || 0;
-    return `rgba(${red},${green},${blue},${alpha})`;
-  }
-
-  throw new Error(`${color} cannot be converted to RGBA color format.`);
+  // If the rgb pattern did not match it must be hex.
+  const red = convertHexToInteger(hex && hex[2]);
+  const green = convertHexToInteger(hex && hex[3]);
+  const blue = convertHexToInteger(hex && hex[4]);
+  return `rgba(${red}, ${green}, ${blue}, ${alpha})`;
 };
 
 export default convertColorToRgba;
