04b82e9ee5c16bde76d2eb6e7e1420371a8ba587
Norbert <nnorb93@gmail.com>
2021-03-09 20:05:45 +0100

    Add useOnScreen hook to IntervalCalendarWeek

--- src/components/IntervalCalendarWeek/index.tsx
+++ src/components/IntervalCalendarWeek/index.tsx
@@ -1,24 +1,30 @@
-import React, { useMemo, useContext } from 'react';
+import React, { useContext, useMemo, useRef, useState, useEffect } from 'react';
 
-import IntervalCalendarDay from '../IntervalCalendarDay';
-
-import { IntervalCalendarWeekProps } from '../../interfaces/IntervalCalendarWeek.interface';
 import Context from '../../context';
 import { getDayAttributes } from '../../helpers';
+import useOnScreen from '../../hooks/useOnScreen';
+import { IntervalCalendarWeekProps } from '../../interfaces/IntervalCalendarWeek.interface';
+import IntervalCalendarDay from '../IntervalCalendarDay';
 import styles from './styles.less';
 
-const IntervalCalendarWeek = ({ numberOfWeek }: IntervalCalendarWeekProps): JSX.Element => {
-  // useContext hooks
-  const { startDate, highlighted, locale } = useContext(Context);
+const DEFAULT_RENDER_TRESHOLD = 8;
 
-  // useMemo hooks
+const IntervalCalendarWeek = ({ numberOfWeek }: IntervalCalendarWeekProps): JSX.Element => {
+  // Hooks
+  const { startDate, highlighted, locale } = useContext(Context);
+  const ref = useRef(null);
+  const isVisible = useOnScreen(ref);
+  const [rendered, setRendered] = useState(numberOfWeek <= DEFAULT_RENDER_TRESHOLD);
   const data = useMemo(() => {
-    if (!startDate) return [];
+    if (!startDate || !rendered) return [];
     return Array.from(Array(7).keys()).map(day => getDayAttributes(startDate, numberOfWeek, day, highlighted, locale));
-  }, [startDate, numberOfWeek, highlighted, locale]);
+  }, [rendered, startDate, numberOfWeek, highlighted, locale]);
+  useEffect(() => {
+    if (isVisible && !rendered) setRendered(true);
+  }, [isVisible, rendered]);
 
   return (
-    <ul key={numberOfWeek} className={styles.week}>
+    <ul ref={ref} key={numberOfWeek} className={styles.week}>
       {data.map(day => (
         <IntervalCalendarDay key={day.key} day={day} />
       ))}
