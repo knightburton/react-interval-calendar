a8d863e0eb86861fc831114e988726a680802ac9
Imre Kiss <jakab.asd@gmail.com>
2021-03-24 22:17:53 +0100

    Move the context initial values into the constants folder

---
+++ src/constants/default-context.ts
@@ -0,0 +1,27 @@
+import * as DEFAULT_PROPS from './default-props';
+
+const DEFAULT_CONTEXT = <ContextType>{
+  customClassNames: DEFAULT_PROPS.CUSTOM_CLASS_NAMES,
+  emptyLabel: DEFAULT_PROPS.EMPTY_LABEL,
+  fadeWeekends: DEFAULT_PROPS.FADE_WEEKENDS,
+  handleSelect: undefined,
+  highlighted: DEFAULT_PROPS.HIGHLIGHTED,
+  highlightedColorAlpha: DEFAULT_PROPS.HIGHLIGHTED_COLOR_ALPHA,
+  locale: DEFAULT_PROPS.LOCALE,
+  numberOfWeekPreRender: DEFAULT_PROPS.NUMBER_OF_WEEK_PRE_RENDER,
+  numberOfWeeks: 0,
+  showGutterBetweenHighlighted: DEFAULT_PROPS.SHOW_GUTTER_BETWEEN_HIGHLIGHTED,
+  showHeaderWeekdays: DEFAULT_PROPS.SHOW_HEADER_WEEKDAYS,
+  showMonthStripes: DEFAULT_PROPS.SHOW_MONTH_STRIPES,
+  showMonths: DEFAULT_PROPS.SHOW_MONTHS,
+  showToday: DEFAULT_PROPS.SHOW_TODAY,
+  showYears: DEFAULT_PROPS.SHOW_YEARS,
+  startDate: undefined,
+  theme: DEFAULT_PROPS.THEME,
+  updateVisibilityMatrix: undefined,
+  visibilityMatrix: {},
+  weekStartsOn: DEFAULT_PROPS.WEEK_STARTS_ON,
+  weeksHeight: DEFAULT_PROPS.HEIGHT,
+};
+
+export default DEFAULT_CONTEXT;
--- src/context/index.ts
+++ src/context/index.ts
@@ -1,29 +1,7 @@
 import { createContext } from 'react';
 
-import * as DEFAULT_PROPS from '../constants/default-props';
+import DEFAULT_CONTEXT from '../constants/default-context';
 
-const Context = createContext<ContextType>({
-  customClassNames: DEFAULT_PROPS.CUSTOM_CLASS_NAMES,
-  emptyLabel: DEFAULT_PROPS.EMPTY_LABEL,
-  fadeWeekends: DEFAULT_PROPS.FADE_WEEKENDS,
-  handleSelect: undefined,
-  highlighted: DEFAULT_PROPS.HIGHLIGHTED,
-  highlightedColorAlpha: DEFAULT_PROPS.HIGHLIGHTED_COLOR_ALPHA,
-  locale: DEFAULT_PROPS.LOCALE,
-  numberOfWeekPreRender: DEFAULT_PROPS.NUMBER_OF_WEEK_PRE_RENDER,
-  numberOfWeeks: 0,
-  showGutterBetweenHighlighted: DEFAULT_PROPS.SHOW_GUTTER_BETWEEN_HIGHLIGHTED,
-  showHeaderWeekdays: DEFAULT_PROPS.SHOW_HEADER_WEEKDAYS,
-  showMonthStripes: DEFAULT_PROPS.SHOW_MONTH_STRIPES,
-  showMonths: DEFAULT_PROPS.SHOW_MONTHS,
-  showToday: DEFAULT_PROPS.SHOW_TODAY,
-  showYears: DEFAULT_PROPS.SHOW_YEARS,
-  startDate: undefined,
-  theme: DEFAULT_PROPS.THEME,
-  updateVisibilityMatrix: () => {},
-  visibilityMatrix: {},
-  weekStartsOn: DEFAULT_PROPS.WEEK_STARTS_ON,
-  weeksHeight: DEFAULT_PROPS.HEIGHT,
-});
+const Context = createContext<ContextType>(DEFAULT_CONTEXT);
 
 export default Context;
