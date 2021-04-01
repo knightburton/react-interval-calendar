f5918f6aee7480aecbfde613e95239603d60a1c9
Imre Kiss <jakab.asd@gmail.com>
2021-04-01 22:25:48 +0200

    Fix the useOnScreen hook intersection instance issue

--- src/hooks/useOnScreen.tsx
+++ src/hooks/useOnScreen.tsx
@@ -1,9 +1,9 @@
-import { useState, useEffect, MutableRefObject } from 'react';
+import { useState, useEffect, useMemo, MutableRefObject } from 'react';
 
 export default (ref: MutableRefObject<Element | null>): boolean => {
   const [isIntersecting, setIntersecting] = useState(false);
 
-  const observer = new IntersectionObserver(([entry]) => setIntersecting(entry.isIntersecting));
+  const observer = useMemo(() => new IntersectionObserver(([entry]) => setIntersecting(entry.isIntersecting)), []);
 
   useEffect(() => {
     if (ref.current) observer.observe(ref.current);
