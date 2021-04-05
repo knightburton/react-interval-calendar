fe4993dc938f8329ecf81d3501888f924a695ce6
Imre Kiss <jakab.asd@gmail.com>
2021-04-05 14:30:20 +0200

    Add test utils to mock out the intersection observer

---
+++ src/tests/test-utils.ts
@@ -0,0 +1,75 @@
+import { act } from '@testing-library/react';
+
+type Item = {
+  callback: IntersectionObserverCallback;
+  elements: Element[];
+  created: number;
+};
+
+const observers = new Map<IntersectionObserver, Item>();
+
+beforeEach(() => {
+  global.IntersectionObserver = jest.fn((callback, options = {}) => {
+    const item = {
+      callback,
+      elements: <Element[]>[],
+      created: Date.now(),
+    };
+
+    const instance: IntersectionObserver = {
+      thresholds: Array.isArray(options.threshold) ? options.threshold : [options.threshold || 0],
+      root: options.root || null,
+      rootMargin: options.rootMargin || '',
+      observe: jest.fn((element: Element) => item.elements.push(element)),
+      unobserve: jest.fn(),
+      disconnect: jest.fn(() => observers.delete(instance)),
+      takeRecords: jest.fn(),
+    };
+
+    observers.set(instance, item);
+
+    return instance;
+  });
+});
+
+afterEach(() => {
+  observers.clear();
+});
+
+const triggerIntersection = (trigger: boolean | number, observer: IntersectionObserver, item: Item): void => {
+  const isIntersecting = typeof trigger === 'number' ? observer.thresholds.some(threshold => trigger >= threshold) : trigger;
+
+  const intersectionRatio = typeof trigger === 'number' ? observer.thresholds.find(threshold => trigger >= threshold) || 0 : (trigger && 1) || 0;
+
+  const entries: IntersectionObserverEntry[] = Array.from(item.elements).map(element => ({
+    boundingClientRect: element.getBoundingClientRect(),
+    intersectionRatio,
+    intersectionRect: isIntersecting
+      ? element.getBoundingClientRect()
+      : {
+          bottom: 0,
+          height: 0,
+          left: 0,
+          right: 0,
+          top: 0,
+          width: 0,
+          x: 0,
+          y: 0,
+          toJSON: () => {},
+        },
+    isIntersecting,
+    rootBounds: null,
+    target: element,
+    time: Date.now() - item.created,
+  }));
+
+  act(() => item.callback(entries, observer));
+};
+
+export const mockAllIsIntersecting = (isIntersecting: boolean | number): void =>
+  observers.forEach((item, observer) => {
+    triggerIntersection(isIntersecting, observer, item);
+  });
+
+export const mockIntersectionInstance = (element: Element): IntersectionObserver | undefined =>
+  Array.from(observers)?.find(observer => observer[1].elements.find(e => e === element))?.[0];
