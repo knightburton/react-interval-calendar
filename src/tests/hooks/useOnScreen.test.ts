8db2965fbbf9bee134d733dd3d3a773259d95709
Imre Kiss <jakab.asd@gmail.com>
2021-04-07 20:12:39 +0200

    Add useOnScreen hook tests

---
+++ src/tests/hooks/useOnScreen.test.ts
@@ -0,0 +1,29 @@
+import { MutableRefObject } from 'react';
+import { renderHook } from '@testing-library/react-hooks';
+import { mockAllIsIntersecting } from '../test-utils';
+import useOnScreen from '../../hooks/useOnScreen';
+
+describe('useOnScreen', () => {
+  test('returns false based on default values', () => {
+    const refMock: MutableRefObject<Element | null> = { current: null };
+    const { result } = renderHook(() => useOnScreen(refMock));
+
+    expect(result.current).toBeFalsy();
+  });
+
+  test('returns false based on non intersecting values', () => {
+    const refMock: MutableRefObject<Element | null> = { current: document.createElement('div') };
+    const { result } = renderHook(() => useOnScreen(refMock));
+    mockAllIsIntersecting(false);
+
+    expect(result.current).toBeFalsy();
+  });
+
+  test('returns true based on intersecting values', () => {
+    const refMock: MutableRefObject<Element | null> = { current: document.createElement('div') };
+    const { result } = renderHook(() => useOnScreen(refMock));
+    mockAllIsIntersecting(true);
+
+    expect(result.current).toBeTruthy();
+  });
+});
