f14301630492d04b611478176b3b5833973f3544
Imre Kiss <jakab.asd@gmail.com>
2023-01-03 23:48:45 +0100

    feat: adapt the example app to the latest changes

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,23 +3,7 @@ import './App.css';
 
 const App = () => (
   <div className="wrapper">
-    <p>Interval Calendar Example</p>
-    <IntervalCalendar
-      weekStartsOn={1}
-      start={new Date(2023, 0, 1)}
-      end={new Date(2023, 11, 31)}
-      highlighted={[
-        { key: 'holiday', start: new Date(2021, 1, 16), end: new Date(2021, 1, 20) },
-        { key: 'cleaning', start: new Date(2021, 2, 24), end: new Date(2021, 3, 9), color: 'rgba(0, 255, 0, 0.4)' },
-      ]}
-      height={700}
-      showMonths
-      showYears
-      showBorder
-      showBorderRadius
-      fadeWeekends
-      onSelect={() => {}}
-    />
+    <IntervalCalendar weekStartsOn={1} start={new Date(2023, 0, 1)} end={new Date(2023, 11, 31)} locale="en-US" bodyHeight={700} onCellClick={() => {}} />
   </div>
 );
 
