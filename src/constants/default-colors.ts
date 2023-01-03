70fc9ec5a28e4228298eea62dfc5f38457a81db4
Imre Kiss <jakab.asd@gmail.com>
2023-01-03 23:49:13 +0100

    feat: get rid of the deprecated components

--- src/constants/default-colors.ts
+++
@@ -1,9 +0,0 @@
-// #spacing.border[primary] colors in rgba format.
-export const HIGHLIGHT_COLORS = {
-  LIGHT: 'rgba(57, 59, 68, 0.2)',
-  DARK: 'rgba(0, 173, 181, 0.2)',
-};
-
-export default {
-  HIGHLIGHT_COLORS,
-};
