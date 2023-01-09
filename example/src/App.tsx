2e88cdfd4dd9a151620e284d8773260bec32fda0
Imre Kiss <jakab.asd@gmail.com>
2023-01-09 21:07:16 +0100

    feat: add more detailed example to the example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,22 +1,28 @@
 import IntervalCalendar from '@knightburton/react-interval-calendar';
 import './App.css';
 
+const getLabel = (data: { isFirstDayOfYear: boolean; isFirstDayOfMonth: boolean; day: string; date: Date }) => {
+  if (data.isFirstDayOfYear) return data.date.toLocaleString('default', { day: '2-digit', month: 'short', year: 'numeric' });
+  if (data.isFirstDayOfMonth) return data.date.toLocaleString('default', { day: '2-digit', month: 'short' });
+  return data.day;
+};
+
 const App = () => (
   <div className="wrapper">
     <IntervalCalendar
       weekStartsOn={1}
       start={new Date(2023, 0, 1)}
       end={new Date(2023, 11, 31)}
-      locale="en-US"
       height={700}
       onCellClick={cell => console.log(cell)}
       containerClassName="container"
       headerContainerClassName="headerContainer"
       headerCellClassName="headerCell"
       headerCellContentClassName="headerCellContent"
-      bodyRowClassName='bodyRow'
+      headerCellContentComponent={({ data, className }) => <span className={className}>{data.long}</span>}
+      bodyRowClassName="bodyRow"
       bodyCellClassName="bodyCell"
-      bodyCellContentComponent={({ data }) => <span className={`${data.isMonthEven ? 'evenMonth' : ''} ${data.isToday ? 'today': ''}`}>{data.day}</span>}
+      bodyCellContentComponent={({ data }) => <span className={`${data.isMonthEven ? 'evenMonth' : ''} ${data.isToday ? 'today' : ''}`}>{getLabel(data)}</span>}
     />
   </div>
 );
